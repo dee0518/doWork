491ed333523d15dd88f1a65a0b335c87b30cce68
dee0518 <dehya0518@naver.com>
2022-03-24 14:10:17 +0900

    로고 하이퍼링크 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+import { MAIN } from '../../navigation/Constant'
 import { Menu, images } from '../../Path'
 
 function Header(){
     return (
         <header>
             <h1 className='logo'>
-                <img src={images['logo.svg']} alt="dee work"/>
+                <Link to={MAIN}>
+                    <img src={images['logo.svg']} alt="dee work"/>
+                </Link>
             </h1>
             <Menu/>
         </header>
