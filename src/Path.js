564bcd3f2780f2b62b897a0a7788a0b859b618dc
dee0518 <dehya0518@naver.com>
2022-03-24 14:07:20 +0900

    scss파일 생성 및 연결

--- src/Path.js
+++ src/Path.js
@@ -1,2 +1,11 @@
+// images
+export { default as images } from './assets/images/importImage'
+
 // components
+export { default as Header } from './components/Header'
 export { default as Menu } from './components/Menu'
+
+// container
+export { default as Schedule } from './container/Schedule'
+
+
