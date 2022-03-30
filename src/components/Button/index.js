f85f56e2f4c497587ad8da16faf2ded4b33e3a4a
dee0518 <dehya0518@naver.com>
2022-03-30 13:04:35 +0900

    버튼 컴포넌트 추가

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+function Button(props){
+    const { className, children } = props
+
+    return (
+        <button type="button" className={className} onClick={(e) => props.onClick(e)}>{children}</button>
+    )
+}
+
+export default Button
