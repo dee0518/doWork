435a2ee2582557a34dc54e8066feeff4e4618061
dee0518 <dehya0518@naver.com>
2022-03-30 13:05:28 +0900

    Wrapper 컴포넌트 추가

---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,11 @@
+function Wrapper(props){
+    const {className, children} = props
+
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    )
+}
+
+export default Wrapper
