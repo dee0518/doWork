3feae87080efed4751f8194237da4f4f762f6c98
dee0518 <dehya0518@naver.com>
2022-04-05 19:37:24 +0900

    라우터 변경

--- src/navigation/Constant.js
+++ src/navigation/Constant.js
@@ -1,7 +1,7 @@
 // route
-export const LOGIN = '/'
-export const MAIN = '/main'
+export const LOGIN = '/deeWork/'
+export const MAIN = '/deeWork/main'
 export const MAINSUB = ':sub/:type/:year/:month/:date'
 
-export const MYPAGE = '/myPage'
+export const MYPAGE = '/deeWork/myPage'
 export const MYPAGESUB = ':sub'
