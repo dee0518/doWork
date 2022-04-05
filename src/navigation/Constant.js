b13e382838ed731b043eac505a61a45693686c45
dee0518 <dehya0518@naver.com>
2022-04-05 19:25:53 +0900

    충돌부분 해결

--- src/navigation/Constant.js
+++ src/navigation/Constant.js
@@ -2,8 +2,6 @@
 export const LOGIN = '/'
 export const MAIN = '/main'
 export const MAINSUB = ':sub/:type/:year/:month/:date'
-<<<<<<< HEAD
+
 export const MYPAGE = '/myPage'
 export const MYPAGESUB = ':sub'
-=======
->>>>>>> 03001b6c23b05247efe98871e66d513d1e20a620
