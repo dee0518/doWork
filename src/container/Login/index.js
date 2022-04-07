345d855ca2b8a6202b7ac46468ea0ae6c042e46b
dee0518 <dehya0518@naver.com>
2022-04-07 11:24:14 +0900

    에러 메세지 수정

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -37,7 +37,19 @@ function Login(){
                 navigator(MAIN)
             })
             .catch((error) => {
-                const errorMessage = error.message;
+                const code = error.code
+                let errorMessage;
+
+                if(code === 'auth/invalid-email'){
+                    errorMessage = '이메일 형식이 아닙니다.'
+                } else if(code === 'auth/user-not-found') {
+                    errorMessage = '가입된 이메일이 아닙니다.'
+                } else if(code === 'auth/wrong-password'){
+                    errorMessage = '비밀번호가 일치하지 않습니다.'
+                } else {
+                    errorMessage = error.message.split('/')[1].split(')')[0];
+                }
+                
                 setError(errorMessage)
             }) 
         } 
@@ -54,7 +66,14 @@ function Login(){
                 const user = result.user;
                 // ...
             }).catch((error) => {
-                const errorMessage = error.message;
+                const code = error.code
+                let errorMessage;
+
+                if(code === 'auth/popup-closed-by-user'){
+                    errorMessage = '구글 로그인 팝업이 닫혔습니다.'
+                } else {
+                    errorMessage = error.message.split('/')[1].split(')')[0];
+                }
 
                 setError(errorMessage)
             });
@@ -68,15 +87,15 @@ function Login(){
             <div className="guide-group">
                 <svg width="363" height="266" viewBox="0 0 363 266" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="135.875" cy="146.725" r="25.9662" fill="white"/>
-                    <path d="M254.443 120.759L202.511 145.473L254.443 172.691" stroke="white" stroke-width="10" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M128.679 191.462C130.302 217.273 154.574 237.763 184.279 237.763C213.984 237.763 238.257 217.273 239.88 191.462H128.679Z" fill="white"/>
-                    <path d="M65.4844 142.658C65.4844 78.3835 117.589 26.2791 181.863 26.2791" stroke="white" stroke-width="10" stroke-linecap="round"/>
-                    <path d="M129.305 58.1893C184.968 26.0522 256.144 45.1238 288.281 100.787" stroke="white" stroke-width="10" stroke-linecap="round"/>
-                    <path d="M89.1517 226.115C39.9146 184.8 33.4919 111.394 74.8062 62.1568" stroke="white" stroke-width="10" stroke-linecap="round"/>
-                    <path d="M305.75 159.551C305.75 202.628 282.346 240.238 247.561 260.361" stroke="white" stroke-width="10" stroke-linecap="round"/>
-                    <path d="M291.376 75.9352C321.836 106.395 331.882 149.538 321.513 188.364" stroke="white" stroke-width="10" stroke-linecap="round"/>
+                    <path d="M254.443 120.759L202.511 145.473L254.443 172.691" stroke="white" strokeWidth="10" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M128.679 191.462C130.302 217.273 154.574 237.763 184.279 237.763C213.984 237.763 238.257 217.273 239.88 191.462H128.679Z" fill="white"/>
+                    <path d="M65.4844 142.658C65.4844 78.3835 117.589 26.2791 181.863 26.2791" stroke="white" strokeWidth="10" strokeLinecap="round"/>
+                    <path d="M129.305 58.1893C184.968 26.0522 256.144 45.1238 288.281 100.787" stroke="white" strokeWidth="10" strokeLinecap="round"/>
+                    <path d="M89.1517 226.115C39.9146 184.8 33.4919 111.394 74.8062 62.1568" stroke="white" strokeWidth="10" strokeLinecap="round"/>
+                    <path d="M305.75 159.551C305.75 202.628 282.346 240.238 247.561 260.361" stroke="white" strokeWidth="10" strokeLinecap="round"/>
+                    <path d="M291.376 75.9352C321.836 106.395 331.882 149.538 321.513 188.364" stroke="white" strokeWidth="10" strokeLinecap="round"/>
                 </svg>
-                <span className="auth-guide">{error? error : '우리 같이 일해보아요!'}</span>
+                <span className={"auth-guide" + (error? ' pink': '')}>{error? error : '우리 같이 일해보아요!'}</span>
             </div>
             <form className="login-form" onSubmit={onSubmit}>
                 <InputTextForm
