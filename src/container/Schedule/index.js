a1fd4d71e54fbc57018cb3a1a292ffdf868d601f
dee0518 <dehya-518@naver.com>
2022-09-16 19:12:29 +0900

    :memo: 레이아웃 및 파이어베이스 관련 오류 수정

--- src/container/Schedule/index.js
+++ src/container/Schedule/index.js
@@ -1,110 +1,139 @@
-import React, { useEffect, useState } from "react"
-import ScheduleView from "./presenter/ScheduleView"
+import React, { useEffect, useState } from "react";
+import ScheduleView from "./presenter/ScheduleView";
 import { addDocument, dbService, getDocuments } from "../../firebase";
-import { collection, query, where, orderBy, Timestamp } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  Timestamp,
+} from "firebase/firestore";
 
-function ScheduleData(props){
-    const { userObj } = props
-    const today = new Date()
-    const [term, setTerm] = useState({
-        started_at: `${today.getFullYear()}-${today.getMonth() + 1}-${1}`,
-        ended_at: `${today.getFullYear()}-${today.getMonth() + 1}-${31}`
-    })
-    const [schedules, setSchedules] = useState([])
+function ScheduleData(props) {
+  const { userObj } = props;
+  const today = new Date();
+  const [term, setTerm] = useState({
+    started_at: `${today.getFullYear()}-${today.getMonth() + 1}-${1}`,
+    ended_at: `${today.getFullYear()}-${today.getMonth() + 1}-${31}`,
+  });
+  const [schedules, setSchedules] = useState([]);
 
-    const addSchedule = async (uid, title, category, started_at, ended_at, started_time, ended_time, participants, content) => {
-        try {
-            await addDocument('schedule', {
-                uid: uid,
-                title: title,
-                category: category,
-                started_at: Timestamp.fromDate(new Date(started_at)),
-                ended_at: '',
-                started_time: started_time,
-                ended_time: ended_time,
-                participants: participants,
-                content: content
-            })
-        } catch (e) {
-                console.error("Error adding document: ", e);
-        } finally {
-            getSchedule(uid, term.started_at, term.ended_at)
+  const addSchedule = async (
+    uid,
+    title,
+    category,
+    started_at,
+    ended_at,
+    started_time,
+    ended_time,
+    participants,
+    content
+  ) => {
+    try {
+      await addDocument("schedule", {
+        uid,
+        title,
+        category,
+        started_at: Timestamp.fromDate(new Date(started_at)),
+        ended_at: "",
+        started_time,
+        ended_time,
+        participants,
+        content,
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    } finally {
+      console.log("finally");
+      getSchedule(uid, term.started_at, term.ended_at);
+    }
+  };
+
+  const getSchedule = async (uid, started_at, endeded_at) => {
+    const scheduleRef = collection(dbService, "schedule");
+    const q = query(
+      scheduleRef,
+      where("uid", "==", uid),
+      where("started_at", ">=", started_at),
+      orderBy("started_at"),
+      orderBy("started_time")
+    );
+
+    const data = await getDocuments(q);
+    let dataGroup = [];
+    if (data.size !== 0) {
+      data.forEach((doc) => {
+        if (new Date(doc.data().started_at.toDate()) <= new Date(endeded_at)) {
+          console.log(doc);
+          dataGroup.push({ id: doc.id, ...doc.data() });
+        } else {
+          return false;
         }
+      });
     }
 
-    const getSchedule = async(uid, started_at, endeded_at) => {
-        const scheduleRef = collection(dbService, 'schedule')
-        const q = query(scheduleRef, 
-            where('uid', '==', uid),
-            where('started_at', '>=' , started_at),
-            orderBy('started_at'),
-            orderBy('started_time')
-        )
+    setSchedules(dataGroup);
+  };
 
-        const data = await getDocuments(q)
-        let dataGroup = []
+  const onAddSchedule = async (newSchedule) => {
+    let uid = userObj.uid;
+    let title = newSchedule.title;
+    let category = newSchedule.category;
+    let started_at = newSchedule.started_at;
+    let ended_at = newSchedule.ended_at;
+    let started_time = newSchedule.started_time;
+    let ended_time = newSchedule.ended_time;
+    let participants = newSchedule.participants;
+    let content = newSchedule.content;
 
-        if(data.size !== 0){
-            data.forEach((doc) => {
-                if(new Date(doc.data().started_at.toDate()) <= new Date(endeded_at)){
-                    dataGroup.push({id: doc.id, ...doc.data()})
-                } else {
-                    return false
-                }
-            })
-        }
+    await addSchedule(
+      uid,
+      title,
+      category,
+      started_at,
+      ended_at,
+      started_time,
+      ended_time,
+      participants,
+      content
+    );
+  };
 
-        setSchedules(dataGroup)
-    }
+  const onSetDate = (selectedDate, curDates) => {
+    let year = selectedDate.getFullYear();
+    let month = selectedDate.getMonth();
 
-    const onAddSchedule = async (newSchedule) => {
-        let uid = userObj.uid
-        let title = newSchedule.title
-        let category = newSchedule.category
-        let started_at = newSchedule.started_at
-        let ended_at = newSchedule.ended_at
-        let started_time = newSchedule.started_time
-        let ended_time = newSchedule.ended_time
-        let participants = newSchedule.participants
-        let content = newSchedule.content
-        
-        await addSchedule(uid, title, category, started_at, ended_at, started_time, ended_time, participants, content)
-    }
+    let s = "";
+    let e = "";
 
-    const onSetDate = (selectedDate, curDates) => {
-        let year = selectedDate.getFullYear()
-        let month = selectedDate.getMonth()
+    if (curDates[0] !== 1) s = new Date(year, month - 1, curDates[0]);
+    else s = new Date(year, month, curDates[0]);
 
-        let s = ''
-        let e = ''
-        
-        if(curDates[0] !== 1) s = new Date(year, month - 1, curDates[0])
-        else  s = new Date(year, month, curDates[0])
+    if (curDates[curDates.length - 1] < 7)
+      e = new Date(year, month + 1, curDates.pop());
+    else e = new Date(year, month, curDates.pop());
 
-        if(curDates[curDates.length - 1] < 7) e = new Date(year, month + 1, curDates.pop())
-        else  e = new Date(year, month, curDates.pop())
+    setTerm({
+      started_at: new Date(s.getFullYear(), s.getMonth(), s.getDate()),
+      ended_at: `${e.getFullYear()}-${e.getMonth() + 1}-${e.getDate()}`,
+    });
+  };
 
-        setTerm({
-            started_at: new Date(s.getFullYear(), s.getMonth(), s.getDate()),
-            ended_at: `${e.getFullYear()}-${e.getMonth() + 1}-${e.getDate()}`
-        })
-    }
+  useEffect(() => {
+    getSchedule(userObj.uid, term.started_at, term.ended_at);
+  }, [term]);
 
-    useEffect(() => {
-        getSchedule(userObj.uid, term.started_at, term.ended_at)
-    },[term])
+  useEffect(() => {
+    return () => getSchedule(userObj.uid, term.started_at, term.ended_at);
+  }, []);
 
-    useEffect(() => {
-        return () => getSchedule(userObj.uid, term.started_at, term.ended_at)
-    },[])
-
-    return (
-        <ScheduleView 
-            scheduleList={schedules}
-            onAddSchedule={onAddSchedule}
-            onSetDate={onSetDate}
-        />
-    )
+  return (
+    <ScheduleView
+      scheduleList={schedules}
+      onAddSchedule={onAddSchedule}
+      onSetDate={onSetDate}
+    />
+  );
 }
 
-export default ScheduleData
+export default ScheduleData;
--- src/container/Schedule/presenter/ScheduleView.js
+++ src/container/Schedule/presenter/ScheduleView.js
@@ -1,231 +1,256 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate, useParams } from 'react-router'
-import { checkboxData, scheduleInit } from "../baseData"
-import { Button, Calendar, SearchForm, SelectBox, Wrapper } from "../../../Path"
-import ScheduleMenu from "./ScheduleMenu"
-import NewScheduleModal from "./NewScheduleModal"
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { checkboxData, scheduleInit } from "../baseData";
+import {
+  Button,
+  Calendar,
+  SearchForm,
+  SelectBox,
+  Wrapper,
+} from "../../../Path";
+import ScheduleMenu from "./ScheduleMenu";
+import NewScheduleModal from "./NewScheduleModal";
 
-function ScheduleView(props){
-    const { scheduleList, onAddSchedule, onSetDate } = props
-    const navigator = useNavigate()
-    const params = useParams()
-    const today = new Date()
-    const list = ['Month', 'Week','Day']
-    const buttonList = [{
-        className: 'close-btn',
-        name: '닫기'
-    },{
-        className: 'save-btn',
-        name: '저장'
-    }]
+function ScheduleView(props) {
+  const { scheduleList, onAddSchedule, onSetDate } = props;
+  const navigator = useNavigate();
+  const params = useParams();
+  const today = new Date();
+  const list = ["Month", "Week", "Day"];
+  const buttonList = [
+    {
+      className: "close-btn",
+      name: "닫기",
+    },
+    {
+      className: "save-btn",
+      name: "저장",
+    },
+  ];
 
-    const [searchValue, setSearchValue] = useState('')
-    const [selectValue, setSelectValue] = useState(params.type)
-    const [checkList,setCheckList] = useState(checkboxData)
-    const [newSchedule, setNewSchedule] = useState(scheduleInit)
-    const [modalState, setModalState] = useState(false)
-    const [alarm, setAlarm] = useState(false)
+  const [searchValue, setSearchValue] = useState("");
+  const [selectValue, setSelectValue] = useState(params.type);
+  const [checkList, setCheckList] = useState(checkboxData);
+  const [newSchedule, setNewSchedule] = useState(scheduleInit);
+  const [modalState, setModalState] = useState(false);
+  const [alarm, setAlarm] = useState(false);
 
-    const onChangeSearch = (e) => setSearchValue(e.target.value)
-    const onChangeSelect = (e) => setSelectValue(e.target.value)   
-    const onClickAddBtn = () => {
-        let pre = new Date()
-        let year = pre.getFullYear()
-        let month = pre.getMonth() + 1 > 9? pre.getMonth() + 1: '0' + (pre.getMonth() + 1)
-        let date = pre.getDate()
-        let hours = pre.getHours() > 9? pre.getHours() : '0' + pre.getHours()
-        let endHours = pre.getHours() + 1 > 9? pre.getHours() + 1 : '0' + (pre.getHours() + 1)
-        let minutes = Math.ceil(pre.getMinutes()/10) * 10 >= 60? '00': Math.ceil(pre.getMinutes()/10) * 10
+  const onChangeSearch = (e) => setSearchValue(e.target.value);
+  const onChangeSelect = (e) => setSelectValue(e.target.value);
+  const onClickAddBtn = () => {
+    let pre = new Date();
+    let year = pre.getFullYear();
+    let month =
+      pre.getMonth() + 1 > 9 ? pre.getMonth() + 1 : "0" + (pre.getMonth() + 1);
+    let date = pre.getDate();
+    let hours = pre.getHours() > 9 ? pre.getHours() : "0" + pre.getHours();
+    let endHours =
+      pre.getHours() + 1 > 9 ? pre.getHours() + 1 : "0" + (pre.getHours() + 1);
+    let minutes =
+      Math.ceil(pre.getMinutes() / 10) * 10 >= 60
+        ? "00"
+        : Math.ceil(pre.getMinutes() / 10) * 10;
 
-        setNewSchedule({...newSchedule, 
-            'started_at' : `${year}-${month}-${date}`,
-            'started_time' : `${hours}:${minutes}`,
-            'ended_time': `${endHours}:${minutes}`
+    setNewSchedule({
+      ...newSchedule,
+      started_at: `${year}-${month}-${date}`,
+      started_time: `${hours}:${minutes}`,
+      ended_time: `${endHours}:${minutes}`,
+    });
+  };
+
+  const onClickAlarmBtn = () => setAlarm(true);
+  const onClickModalBtn = (e) => {
+    if (e.target.textContent === "저장") {
+      if (newSchedule.title === "" || newSchedule.category === "") {
+      } else {
+        onAddSchedule(newSchedule);
+      }
+    }
+
+    if (
+      (newSchedule.title !== "" && newSchedule.category !== "") ||
+      e.target.textContent === "닫기"
+    ) {
+      setModalState(false);
+      setNewSchedule(scheduleInit);
+    }
+  };
+
+  const onClickSubTitle = (title) => {
+    let url;
+
+    if (Object.keys(params).length === 0) {
+      url = `${title}/month/${today.getFullYear()}/${
+        today.getMonth() + 1
+      }/${today.getDate()}`;
+    } else {
+      url = `${title}/${params.type}/${params.year}/${params.month}/${params.date}`;
+    }
+
+    navigator(url);
+  };
+
+  const onChangeNewSchedule = (e) => {
+    let name = e.target.name;
+    let value;
+
+    if (name === "category") {
+      value = e.target.id;
+    } else {
+      value = e.target.value;
+    }
+
+    let ended_time = newSchedule.ended_time;
+
+    if (
+      name === "started_time" &&
+      Number(value.split(":")[0]) >= Number(ended_time.split(":")[0])
+    ) {
+      ended_time = value
+        .split(":")
+        .map((t, i) => {
+          if (i === 0) return Number(t) + 1 > 23 ? "00" : Number(t) + 1;
+          else return t;
         })
+        .join(":");
+
+      setNewSchedule({ ...newSchedule, [name]: value, ended_time: ended_time });
+    } else {
+      setNewSchedule({ ...newSchedule, [name]: value });
     }
+  };
 
-    const onClickAlarmBtn = () => setAlarm(true)
-    const onClickModalBtn = (e) => {
-        if(e.target.textContent === '저장'){
-            if(newSchedule.title === '' || newSchedule.category === '') {
-
-            } else {
-                onAddSchedule(newSchedule)
-            }
-        } 
-        
-        if((newSchedule.title !== '' && newSchedule.category !== '') || e.target.textContent === '닫기') {
-            setModalState(false)
-            setNewSchedule(scheduleInit)
-        }   
+  const onAddParticipants = () => {
+    if (newSchedule.participants.length < 8) {
+      setNewSchedule({
+        ...newSchedule,
+        participant: "",
+        participants: [...newSchedule.participants, newSchedule.participant],
+      });
+    } else {
+      setNewSchedule({ ...newSchedule, participant: "" });
     }
+  };
 
-    const onClickSubTitle = (title) => {
-        let url;
+  const onDeleteParticipant = (name) => {
+    let filterGroup = newSchedule.participants.filter((v) => v != name);
 
-        if(Object.keys(params).length === 0){
-            url = `${title}/month/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`
-        } else {
-            url = `${title}/${params.type}/${params.year}/${params.month}/${params.date}`
+    setNewSchedule({ ...newSchedule, participants: filterGroup });
+  };
+
+  const onChageCheckList = (e) => {
+    let id = Number(e.target.id.split("_")[1]);
+
+    let curChk = checkList[id - 1].checked;
+    let restChk = checkList.filter(
+      (v, i) => i !== 0 && i != id - 1 && v.checked === true
+    ).length;
+
+    setCheckList(
+      checkList.map((check) => {
+        if (id === 1) {
+          return { ...check, checked: !checkList[0].checked };
+        } else if (check.id === id) {
+          return { ...check, checked: !check.checked };
         }
 
-        navigator(url)
-    }
-
-    const onChangeNewSchedule = (e) => {
-        let name = e.target.name
-        let value
-
-        if(name === 'category'){
-            value = e.target.id
-        } else{
-            value = e.target.value
+        if (check.id === 1) {
+          if (curChk) {
+            return { ...check, checked: false };
+          } else if (restChk === 3) {
+            return { ...check, checked: true };
+          }
         }
 
-        let ended_time = newSchedule.ended_time
+        return check;
+      })
+    );
+  };
 
-        if(name === 'started_time' &&
-            Number(value.split(':')[0]) >= Number(ended_time.split(':')[0])
-        ){
-            ended_time = value.split(':').map((t,i)=> {
-                if(i === 0) return Number(t) + 1 > 23? '00': Number(t) + 1
-                else return t
-            }).join(':')
+  useEffect(() => {
+    if (newSchedule.started_at !== "" && !modalState) setModalState(true);
+  }, [newSchedule]);
 
-            setNewSchedule({...newSchedule, [name] : value, 'ended_time': ended_time})
-        } else {
-            setNewSchedule({...newSchedule, [name] : value})
-        }
+  useEffect(() => {
+    if (selectValue === "") return;
+
+    let url;
+    if (Object.keys(params).length === 0) {
+      url = `calendar/${selectValue}/${today.getFullYear()}/${
+        today.getMonth() + 1
+      }/${today.getDate()}`;
+    } else {
+      url = `${params.sub}/${selectValue}/${params.year}/${params.month}/${params.date}`;
     }
 
-    const onAddParticipants = () => {
-        if(newSchedule.participants.length < 8){
-            setNewSchedule({...newSchedule, 
-                'participant': '',
-                'participants' : [...newSchedule.participants, newSchedule.participant]
-            })
-        } else {
-            setNewSchedule({...newSchedule, 
-                'participant': ''
-            })
-        } 
-    } 
+    navigator(url);
+  }, [selectValue]);
 
-    const onDeleteParticipant = (name) => {
-        let filterGroup = newSchedule.participants.filter(v => v != name)
-
-        setNewSchedule({...newSchedule,
-            'participants' : filterGroup
-        })
+  useEffect(() => {
+    if (Object.keys(params).length === 0) {
+      setSelectValue("month");
+    } else if (params.type === "day") {
+      setSelectValue(params.type);
     }
+  }, [params]);
 
-    const onChageCheckList = (e)=> {
-        let id = Number(e.target.id.split('_')[1])
-
-        let curChk = checkList[id - 1].checked
-        let restChk = checkList.filter((v,i) => i !== 0 && i != id - 1 && v.checked === true).length
-
-        setCheckList(checkList.map((check) => {
-            if(id === 1){
-                return {...check, checked: !checkList[0].checked}
-            } else if(check.id === id){
-                return {...check, checked: !check.checked}
-            }
-
-            if(check.id === 1){
-                if(curChk){
-                    return {...check, checked: false}
-                } else if(restChk === 3) {
-                    return {...check, checked: true}
-                }
-            }
-            
-            return check
-        }))
-    }
-
-    useEffect(() => {
-        if(newSchedule.started_at !== '' && !modalState) setModalState(true)
-    },[newSchedule])
-
-    useEffect(() => {
-        if(selectValue === '') return 
-
-        let url;
-        if(Object.keys(params).length === 0){
-            url = `calendar/${selectValue}/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`
-        } else {
-            url = `${params.sub}/${selectValue}/${params.year}/${params.month}/${params.date}`
-        }
-
-        navigator(url)
-    },[selectValue])
-  
-
-    useEffect(() => {
-        if(Object.keys(params).length === 0){
-            setSelectValue('month')
-        } else if(params.type === 'day'){
-            setSelectValue(params.type)
-        }
-    },[params])
-
-    return (
-        <Wrapper className="schedule-view-wrapper">
-            <ScheduleMenu
-                onClickSubTitle={onClickSubTitle}
-                params={params}
-                today={today} 
-                scheduleList={scheduleList}
-                onSetDate={onSetDate}
-                checkList={checkList}
-                onChange={onChageCheckList}
-            />
-            <Wrapper className="search-cal-group">
-                <Wrapper className="top-group">
-                    <SearchForm
-                        value={searchValue} 
-                        onChange={onChangeSearch}
-                    />
-                    <Button className={'alarm-btn'} onClick={onClickAlarmBtn}>Alarm</Button>
-                </Wrapper>
-                <Wrapper className="select-add-group">
-                    <SelectBox 
-                        id={'calendar-type'} 
-                        labelClass={'blind'}
-                        labelText={'calendar-show-type'}
-                        list={list} 
-                        value={selectValue} 
-                        onChange={onChangeSelect}
-                    />
-                    <Button className={'add-btn'} onClick={onClickAddBtn}>Add</Button>
-                </Wrapper>
-                <Calendar 
-                    styleType={'grid'} 
-                    params={params}
-                    today={today} 
-                    scheduleList={scheduleList}
-                    onSetDate={onSetDate}
-                    checkList={checkList}
-                />
-            </Wrapper>  
-
-            {/* modal */}
-            {
-                modalState && 
-                <NewScheduleModal 
-                    buttonList={buttonList} 
-                    newSchedule={newSchedule}
-                    onClickModalBtn={onClickModalBtn}
-                    onChange={onChangeNewSchedule}
-                    onAddParticipants={onAddParticipants}
-                    onDeleteParticipant={onDeleteParticipant}
-                />
-            }
-
-            {/* alarm */}
+  return (
+    <Wrapper className="schedule-view-wrapper">
+      <ScheduleMenu
+        onClickSubTitle={onClickSubTitle}
+        params={params}
+        today={today}
+        scheduleList={scheduleList}
+        onSetDate={onSetDate}
+        checkList={checkList}
+        onChange={onChageCheckList}
+      />
+      <Wrapper className="search-cal-group">
+        <Wrapper className="top-group">
+          <SearchForm value={searchValue} onChange={onChangeSearch} />
+          <Button className={"alarm-btn"} onClick={onClickAlarmBtn}>
+            Alarm
+          </Button>
         </Wrapper>
-    )
+        <Wrapper className="select-add-group">
+          <SelectBox
+            id={"calendar-type"}
+            labelClass={"blind"}
+            labelText={"calendar-show-type"}
+            list={list}
+            value={selectValue}
+            onChange={onChangeSelect}
+          />
+          <Button className={"add-btn"} onClick={onClickAddBtn}>
+            Add
+          </Button>
+        </Wrapper>
+        <Calendar
+          styleType={"grid"}
+          params={params}
+          today={today}
+          scheduleList={scheduleList}
+          onSetDate={onSetDate}
+          checkList={checkList}
+        />
+      </Wrapper>
+
+      {/* modal */}
+      {modalState && (
+        <NewScheduleModal
+          buttonList={buttonList}
+          newSchedule={newSchedule}
+          onClickModalBtn={onClickModalBtn}
+          onChange={onChangeNewSchedule}
+          onAddParticipants={onAddParticipants}
+          onDeleteParticipant={onDeleteParticipant}
+        />
+      )}
+
+      {/* alarm */}
+    </Wrapper>
+  );
 }
 
-export default ScheduleView
+export default ScheduleView;
