92dbfb2c552535f6c1efb80540cbae530331aeee
dee0518 <dehya-518@naver.com>
2022-11-16 17:41:26 +0900

    :art: sub menu 마크업 수정

--- src/feature/SubMenu.tsx
+++ src/feature/SubMenu.tsx
@@ -1,30 +1,14 @@
 import { ReactNode } from 'react';
-import { Link } from 'react-router-dom';
 
 interface SubMenuProps {
-  isLinked: boolean;
-  titles: string[];
+  title: string;
   children: ReactNode;
 }
 
 const SubMenu = (props: SubMenuProps) => {
   return (
     <div className="subMenu">
-      <div className="subMenu__title__group">
-        {props.titles.map(title =>
-          props.isLinked ? (
-            <h2 key={title}>
-              <Link className="subMenu__title" to={title}>
-                {title}
-              </Link>
-            </h2>
-          ) : (
-            <h2 className="subMenu__title" key={title}>
-              {title}
-            </h2>
-          )
-        )}
-      </div>
+      <h2 className="subMenu__title">{props.title}</h2>
       {props.children}
     </div>
   );
