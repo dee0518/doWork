5d4bcf58931755a6af31d243abc3cf2e22f74500
dee0518 <dehya-518@naver.com>
2022-11-21 23:02:53 +0900

    :memo: 배포를 위한 코드 푸쉬

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
+      favicon: path.resolve(__dirname, 'public/favicon.ico'),
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
